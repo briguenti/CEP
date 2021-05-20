364a9101bb9cdcbda95c8f2bfb118ed626b1c73a
Vinicius Briguenti <vinicius_kaneki@hotmail.com>
2021-05-20 18:00:05 -0300

    Finalizado busca de CEP com Ajax.

---
+++ ajax-com-js-puro-master/javascript.js
@@ -0,0 +1,20 @@
+function buscaCep(e) {
+    e.preventDefault();
+    let inputCep = document.querySelector('input[name=cep]').value;
+    let url = 'http://viacep.com.br/ws/' + inputCep + '/json';
+    let xhr = new XMLHttpRequest();  
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = function() { 
+        if (xhr.readyState == 4 && xhr.status == 200) { 
+            preencheCampos(JSON.parse(xhr.responseText)); }
+         } 
+    xhr.send();
+}
+
+function preencheCampos(json) { 
+    document.querySelector('input[name=endereco]').value = json.logradouro;
+    document.querySelector('input[name=bairro]').value = json.bairro; 
+    document.querySelector('input[name=complemento]').value = json.complemento;
+    document.querySelector('input[name=cidade]').value = json.localidade; 
+    document.querySelector('input[name=estado]').value = json.uf; 
+}
